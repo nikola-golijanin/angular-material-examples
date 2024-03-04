e024bdfcf93095420f843a958a2752ea38942ad2
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-03-04 11:45:43 +0100

    Section 3: Angular Material Dialogs

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import {openEditCourseDialog} from "../course-dialog/course-dialog.component";
+import {filter} from "rxjs/operators";
 
 @Component({
     selector: 'courses-card-list',
@@ -12,18 +14,18 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor() {
+    constructor(private dialog:MatDialog) {
     }
 
     ngOnInit() {
-
     }
 
     editCourse(course:Course) {
-
-
+       openEditCourseDialog(this.dialog,course)
+         .pipe(
+           filter(val => !!val))
+         .subscribe(value => console.log("NEW COURSE VALUE",value))
     }
-
 }
 
 
